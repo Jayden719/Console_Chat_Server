c58fc938ca8a8e2ee2694d5141533b23d9585c38
피글렛을지향하라 <118239596+Jayden719@users.noreply.github.com>
2024-08-11 22:20:33 +0900

    초기 커밋.

---
+++ MyServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatConsole_s
+{
+    class MyServer
+    {
+        public MyServer()
+        {
+            AsyncServerStart();
+
+        }
+
+        private void AsyncServerStart()
+        {
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Any, 9999));
+            listener.Start();
+            Console.WriteLine("서버 구동 / 클라이언트 접속 대기...");
+
+            while (true)
+            {
+            TcpClient acceptClient = listener.AcceptTcpClient();
+            Console.WriteLine("클라이언트 접속 성공");
+
+            ClientData clientData = new ClientData(acceptClient);
+
+            clientData.client.GetStream().BeginRead(clientData.readByteData, 0, clientData.readByteData.Length, new AsyncCallback(DataReceived), clientData);
+            }
+
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            ClientData callbackClient = ar.AsyncState as ClientData;
+
+            int bytesRead = callbackClient.client.GetStream().EndRead(ar);
+
+            String readString = Encoding.Default.GetString(callbackClient.readByteData, 0, bytesRead);
+
+            Console.WriteLine("{0}번 사용자 : {1}", callbackClient.clientNumber, readString);
+
+            callbackClient.client.GetStream().BeginRead(callbackClient.readByteData, 0, callbackClient.readByteData.Length, new AsyncCallback(DataReceived), callbackClient);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ChatConsole_s;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        MyServer a = new MyServer();
+
+        /*Console.WriteLine("서버콘솔창 \n\n");
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+        TcpListener server = new TcpListener(ip, 9999);
+
+        server.Start();
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("클라이언트 접속");      
+
+        while (true)
+        {
+            byte[] byteData = new byte[1024];
+            client.GetStream().Read(byteData, 0, byteData.Length);
+            String stringData = Encoding.Default.GetString(byteData);
+
+            int endP = stringData.IndexOf('\0');
+            String msg = stringData.Substring(0, endP + 1);
+
+            Console.WriteLine(msg);
+        }*/
+    }
+}
